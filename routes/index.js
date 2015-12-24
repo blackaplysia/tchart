10ff2521cafed8a82b967c271691db56388e2ada
Miki Yutani <blackaplysia@gmail.com>
2015-12-24 12:56:59 +0900

    Rewrite output messages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Chart for T* people' });
+  res.render('index', { title: 'Career Chart for T* people' });
 });
 
 module.exports = router;
